a61c8d21fefe8c12f00b6c6ca834d175a75683e5
ArchangeH <lugagne.theo@gmail.com>
2023-11-17 10:23:19 +0100

    Implement class

--- class/Patient.php
+++ class/Patient.php
@@ -2,7 +2,108 @@
 
 namespace class;
 
-class Patient
-{
+use DateTime;
 
+class Patient extends Personne
+{
+    private int $numeroDeSecu;
+    private String $addresse;
+    private DateTime $dateDeNaisance;
+    private string $lieuDeNaissance;
+
+    private Medecin $medecinRefferent;
+
+
+    public function __construct(int $numeroDeSecu,string $nom, string $prenom, Civilite $civilite, String $addresse,
+                                DateTime $dateDeNaisance, string $lieuDeNaissance, Medecin $medecinRefferent=null)
+    {
+        parent::__construct($nom, $prenom, $civilite);
+        $this->numeroDeSecu = $numeroDeSecu;
+        $this->addresse = $addresse;
+        $this->dateDeNaisance = $dateDeNaisance;
+        $this->lieuDeNaissance = $lieuDeNaissance;
+        if ($this->medecinRefferent == null) {
+            $this->medecinRefferent = $medecinRefferent;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumeroDeSecu(): int
+    {
+        return $this->numeroDeSecu;
+    }
+
+    /**
+     * @return String
+     */
+    public function getAddresse(): String
+    {
+        return $this->addresse;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateDeNaisance(): DateTime
+    {
+        return $this->dateDeNaisance;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLieuDeNaissance(): string
+    {
+        return $this->lieuDeNaissance;
+    }
+
+    /**
+     * @return Medecin
+     */
+    public function getMedecinRefferent(): Medecin
+    {
+        return $this->medecinRefferent;
+    }
+
+    /**
+     * @param int $numeroDeSecu
+     */
+    public function setNumeroDeSecu(int $numeroDeSecu): void
+    {
+        $this->numeroDeSecu = $numeroDeSecu;
+    }
+
+    /**
+     * @param String $addresse
+     */
+    public function setAddresse(String $addresse): void
+    {
+        $this->addresse = $addresse;
+    }
+
+    /**
+     * @param DateTime $dateDeNaisance
+     */
+    public function setDateDeNaisance(DateTime $dateDeNaisance): void
+    {
+        $this->dateDeNaisance = $dateDeNaisance;
+    }
+
+    /**
+     * @param string $lieuDeNaissance
+     */
+    public function setLieuDeNaissance(string $lieuDeNaissance): void
+    {
+        $this->lieuDeNaissance = $lieuDeNaissance;
+    }
+
+    /**
+     * @param Medecin $medecinRefferent
+     */
+    public function setMedecinRefferent(Medecin $medecinRefferent): void
+    {
+        $this->medecinRefferent = $medecinRefferent;
+    }
 }
--- class/RDV.php
+++ class/RDV.php
@@ -2,7 +2,85 @@
 
 namespace class;
 
+use DateTime;
+
 class RDV
 {
+    private Patient $patient;
+    private Medecin $medecin;
 
+    private DateTime $dateHeure;
+    private int $dureeEnMinute;
+
+    public function __construct(Patient $patient, Medecin $medecin, DateTime $dateHeure, int $dureeEnMinute)
+    {
+        $this->patient = $patient;
+        $this->medecin = $medecin;
+        $this->dateHeure = $dateHeure;
+        $this->dureeEnMinute = $dureeEnMinute;
+    }
+
+    /**
+     * @return Medecin
+     */
+    public function getMedecin(): Medecin
+    {
+        return $this->medecin;
+    }
+
+    /**
+     * @return Patient
+     */
+    public function getPatient(): Patient
+    {
+        return $this->patient;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateHeure(): DateTime
+    {
+        return $this->dateHeure;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDureeEnMinute(): int
+    {
+        return $this->dureeEnMinute;
+    }
+
+    /**
+     * @param Medecin $medecin
+     */
+    public function setMedecin(Medecin $medecin): void
+    {
+        $this->medecin = $medecin;
+    }
+
+    /**
+     * @param Patient $patient
+     */
+    public function setPatient(Patient $patient): void
+    {
+        $this->patient = $patient;
+    }
+
+    /**
+     * @param DateTime $dateHeure
+     */
+    public function setDateHeure(DateTime $dateHeure): void
+    {
+        $this->dateHeure = $dateHeure;
+    }
+
+    /**
+     * @param int $dureeEnMinute
+     */
+    public function setDureeEnMinute(int $dureeEnMinute): void
+    {
+        $this->dureeEnMinute = $dureeEnMinute;
+    }
 }
