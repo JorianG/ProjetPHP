af8560a043f7af3b78107290d888c84b8d8a2d56
jorian <jorian.gouagout@etu.iut-tlse3.fr>
2023-11-17 11:04:42 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `Personne` (
+            `Id_Personne` AUTO_INCREMENT NOT NULL,
+            `Nom` VARCHAR(50) NOT NULL,
+            `Prenom` VARCHAR(50)NOT NULL,
+            `Civilite` VARCHAR(3) NOT NULL,
+            PRIMARY KEY(Id_Personne)
+        );
+
+CREATE TABLE `Personne` (
+            `Id_Personne` AUTO_INCREMENT NOT NULL,
+            `Nom` VARCHAR(50) NOT NULL,
+            `Prenom` VARCHAR(50)NOT NULL,
+            `Civilite` VARCHAR(3) NOT NULL,
+            PRIMARY KEY(Id_Personne)
+        );
