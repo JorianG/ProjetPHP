6efb2c3fefb499d0e27cfcb6255cfa1620ab07e4
jorian <jorian.gouagout@etu.iut-tlse3.fr>
2023-11-17 11:05:04 +0100

    Auto stash before merge of "main" and "origin/main"

--- GenerationTables.php
+++ GenerationTables.php
@@ -3,17 +3,17 @@
     function creerTables() {
         $db = connexion();
 
-        $sql = "";
-        $sql .= "CREATE TABLE IF NOT EXISTS `Personne` (
-            `Id_Personne` COUNTER NOT NULL,
+    
+        $sql = "CREATE TABLE `Personne` (
+            `Id_Personne` AUTO_INCREMENT NOT NULL,
             `Nom` VARCHAR(50) NOT NULL,
             `Prenom` VARCHAR(50)NOT NULL,
             `Civilite` VARCHAR(3) NOT NULL,
             PRIMARY KEY(Id_Personne)
         );";
+        $db->exec($sql);
 
-
-        $sql .= "CREATE TABLE IF NOT EXISTS `Patient` (
+        $sql = "CREATE TABLE `Patient` (
             `Id_Personne` INT NOT NULL,
             `Num_secu` VARCHAR(13) NOT NULL,
             `Adresse` VARCHAR(150) NOT NULL,
@@ -24,28 +24,29 @@
             FOREIGN KEY(`Id_Personne`) REFERENCES `Personne`(`Id_Personne`),
             FOREIGN KEY(`Id_Personne_Id_medeciRef`) REFERENCES `Medecin`(`Id_Personne`)
         );";
+        $db->exec($sql);
 
-
-        $sql .= "CREATE TABLE IF NOT EXISTS `RDV` (
+        $sql = "CREATE TABLE `RDV` (
             `Id_Personne_id_medecin` INT NOT NULL,
             `Id_Personne_Id_Patient` INT NOT NULL,
-            `Id_RDV` COUNTER NOT NULL,
+            `Id_RDV` AUTO_INCREMENT NOT NULL,
             `DateHeure` DATETIME NOT NULL,
             `DureeEnM` INT NOT NULL,
             PRIMARY KEY(`Id_Personne_id_medecin`, `Id_Personne_Id_Patient`, `Id_RDV`),
             FOREIGN KEY(`Id_Personne_id_medecin`) REFERENCES `Medecin`(`Id_Personne`),
             FOREIGN KEY(`Id_Personne_Id_Patient`) REFERENCES `Patient`(`Id_Personne`)
         );";
+        $db->exec($sql);
 
-
-        $sql .= "CREATE TABLE IF NOT EXISTS `Medecin` (
+        $sql = "CREATE TABLE `Medecin` (
             `Id_Personne` INT NOT NULL,
             `Specialite` VARCHAR(50) NOT NULL,
             PRIMARY KEY(`Id_Personne`),
             FOREIGN KEY(`Id_Personne`) REFERENCES `Personne`(`Id_Personne`)
         );";
-
         $db->exec($sql);
+        
+        echo "<script>console.log('Tables crées');</script>";
     }
 ?>
 
