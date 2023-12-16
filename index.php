ed5ff542716eac9a0efbb6a3d41918fc43fef0d1
jorian <jorian.gouagout@etu.iut-tlse3.fr>
2023-12-16 12:41:09 +0100

    index -> patient

--- index.php
+++ index.php
@@ -6,18 +6,19 @@
     if (function_exists('customPageHeader')){
       customPageHeader();
     }?>
-                
-                <div class=" container d-flex bg-danger justify-content-evenly" style="margin-top: 18px;">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="container">
+                <div class=" d-flex  justify-content-evenly row" style="margin-top: 18px;">
+
+                    <div class="card col-4" style="width: 18rem;">
+                        <img class="card-img-top img-thumbnail" src="./assets/patient.png" alt="Card image cap">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <h5 class="card-title">Gestion des patients</h5>
+                                <p class="card-text">Liste des patients de l'hopital permettant d'afficher leur profil ainsi que leur médecin responsable.</p>
+                                <a href="./patients.php" class="btn btn-primary">Voir la liste</a>
                             </div>
                     </div>
                     
-                    <div class="card" style="width: 18rem;">
+                    <div class="card col-4" style="width: 18rem;">
                         <img class="card-img-top" src="..." alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
@@ -26,7 +27,18 @@
                             </div>
                     </div>
 
-                    <div class="card" style="width: 18rem;">
+                    <div class="card col-4" style="width: 18rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+                <div class=" d-flex  justify-content-evenly row" style="margin-top: 18px;">
+
+                    <div class="card col-4" style="width: 18rem;">
                         <img class="card-img-top" src="..." alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
@@ -35,9 +47,7 @@
                             </div>
                     </div>
 
-                   
-
-                    <div class="card" style="width: 18rem;">
+                    <div class="card col-4" style="width: 18rem;">
                         <img class="card-img-top" src="..." alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
@@ -46,7 +56,7 @@
                             </div>
                     </div>
 
-                    <div class="card" style="width: 18rem;">
+                    <div class="card col-4" style="width: 18rem;">
                         <img class="card-img-top" src="..." alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
@@ -54,15 +64,7 @@
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                     </div>
-
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                    </div>
+                </div>
                 </div>
                 </body>
                 </html>
