ce98e093302b934d7a2d2f6758b7f5705bf90848
jorian <jorian.gouagout@etu.iut-tlse3.fr>
2023-12-17 20:17:59 +0100

    push

--- header.php
+++ header.php
@@ -1,12 +1,13 @@
 <?php
- function customPageHeader(){?>
+ function customPageHeader($title){?>
  <head>
  <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <title></title>
  <meta name="description" content="">
  <meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/style.css">
+
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
@@ -25,7 +26,7 @@
                      <span class="navbar-toggler-icon"></span>
                  </button>
                  
-         <h1 class="navbar-text  titre-page">Title</h1>
+         <h1 class="navbar-text  titre-page"><?php echo $title ?></h1>
              <div class="d-flex container-profil">
                  <p class="navbar-text navbar nom_docteur">Dr Magnaud</p>
                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle navbar" viewBox="0 0 16 16">
