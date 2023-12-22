2bf3d5b097f213726b4f9f833f830f3169625e61
Théo Lugagne <lugagne.theo@gmail.com>
2023-12-22 11:09:15 +0100

    implementation de DAOMedecin

--- repositoring/MedecinDAO.php
+++ repositoring/MedecinDAO.php
@@ -1,8 +1,32 @@
 <?php
 
 namespace repositoring;
+use class\Medecin;
+use service\PersonneService;
+use PDO;
 
 class MedecinDAO
 {
+    private PDO $db;
+    private PersonneService $personneService;
+
+    public function __construct()
+    {
+        $this->db = connexion();
+    }
+
+    public function insert(Medecin $m)
+    {
+        $sql = "INSERT INTO Medecin VALUES (".$m->getIdPersonne().");";
+        $this->db->exec($sql);
+    }
+
+    public function update(Medecin $m)
+    {
+        $this->personneService = new PersonneService($m);
+        $this->personneService->update();
+    }
+
+
 
 }
