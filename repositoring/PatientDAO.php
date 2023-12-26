a8ae974358fc791a85b4beed1d7fcc509f9aa776
jorian <jorian.gouagout@etu.iut-tlse3.fr>
2023-12-26 18:28:59 +0100

    Init of DAO Patient first iteration

---
+++ repositoring/PatientDAO.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace repositoring;
+
+use class\Patient;
+use class\Personne;
+use PDO;
+include_once $_SERVER['DOCUMENT_ROOT']."/ProjetPHP/class/Patient.php";
+include_once $_SERVER['DOCUMENT_ROOT']."/ProjetPHP/PDO.php";
+
+class PersonneDAO
+{
+    private PDO $db;
+
+    public function __construct()
+    {
+        $this->db = getInstance();
+    }
+
+    public function insert(Patient $p)
+    {
+        
+        //TODO : SQL Injection protection please
+        $sql = "INSERT INTO Patient VALUES ((SELECT Id_Personne FROM personne WHERE Nom = ? AND Prenom = ?), '".$p->getNumeroDeSecu()."', '".$p->getAdresse()."', '".$p->getDateDeNaisance()."', '".$p->getLieuDeNaissance()."', '".$p->getMedecinRefferent()."');";
+        $this->db->exec($sql);
+    }
+
+    public function update(Patient $p, int $id_personne)
+    {
+        $sql = "UPDATE patient SET Num_Secu = '".$p->getNumeroDeSecu()."', Adresse = '".$p->getAdresse()."', DateNaissance = '".$p->getDateDeNaisance()."', LieuDeNaissance = '".$p->getLieuDeNaissance()."', Id_Personne_Id_medecinRef = '".$p->getMedecinRefferent()."' WHERE Id_Personne = ".$id_personne.";";
+        $this->db->exec($sql);
+    }
+
+    public function delete(int $id_personne)
+    {
+        $sql = "DELETE FROM Patient WHERE Id_Personne = ".$id_personne.";";
+        $this->db->exec($sql);
+    }
+
+    public function selectById(int $id_personne): mixed
+    {
+        $sql = "SELECT * FROM Patient WHERE Id_Personne = ".$id_personne.";";
+        $result = $this->db->query($sql);
+        return $result->fetch();
+    }
+
+    public function selectAll(): array|false
+    {
+        $sql = "SELECT * FROM Patient;";
+        $result = $this->db->query($sql);
+        return $result->fetchAll();
+    }
+
+    public function selectAllByMedecin(int $id_medecin): array|false
+    {
+        $sql = "SELECT * FROM Patient WHERE Id_Personne_Id_medecinRef = ".$id_medecin.";";
+        $result = $this->db->query($sql);
+        return $result->fetchAll();
+    }
+    
+}
