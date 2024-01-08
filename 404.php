d4610cabd8d0d60099d3622f9c9e006ff94f3a3c
Théo Lugagne <lugagne.theo@gmail.com>
2024-01-08 20:25:39 +0100

    add 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+echo "<!DOCTYPE html>
+    <html lang=\"fr\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>404</title>
+    </head>
+    <body>
+        <h1>404</h1>
+        <p>La page demandée n'existe pas</p>
+    </body>
+    </html>";
